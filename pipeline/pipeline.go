23c8d5087c713ef80c8e6b54bb670a4100712636
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2022-12-29 14:47:19 +0700

    feat: add pipeline

---
+++ pipeline/pipeline.go
@@ -0,0 +1,34 @@
+package pipeline
+
+import (
+	"context"
+)
+
+type ctxkey string
+
+const (
+	CTXKEY_REQ ctxkey = "pipeline.request"
+	CTXKEY_RES ctxkey = "pipeline.response"
+)
+
+type Pipe func(ctx context.Context) (context.Context, error)
+type Pipeline func(Pipe) Pipe
+
+func New(pipelines []Pipeline) Pipe {
+	pipe := func(ctx context.Context) (context.Context, error) {
+		return ctx, nil
+	}
+
+	// we want to  get result in order of pipeline definition,
+	// so we need to prepare it in reverse order
+	for i := len(pipelines) - 1; i >= 0; i-- {
+		pipe = pipelines[i](pipe)
+	}
+
+	return pipe
+}
+
+type BatchResult struct {
+	Key   string
+	Error string
+}
