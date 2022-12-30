ee303043132e27e26b7258b22269028d4d84333c
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2022-12-30 21:02:39 +0700

    refactor: remove redundant errors

--- msgbus/errors.go
+++ msgbus/errors.go
@@ -1,8 +1,8 @@
 package msgbus
 
+import "errors"
+
 var (
-	ErrEventIdWasSet     = errors.New("msgbuss: event id has set already")
-	ErrEventTsWasSet     = errors.New("msgbuss: event timestamps has set already")
-	ErrEventBucketWasSet = errors.New("msgbuss: event bucket has set already")
-	ErrEventDataWasSet   = errors.New("msgbuss: event data has set already")
+	ErrEventIdWasSet   = errors.New("msgbuss: event id has set already")
+	ErrEventDataWasSet = errors.New("msgbuss: event data has set already")
 )
