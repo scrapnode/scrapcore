f393dc8385c75c8c2e513c705175c850dc3a74f7
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-02 10:09:10 +0700

    feat: add headers utils

--- transport/http_request.go
+++ transport/http_request.go
@@ -14,3 +14,20 @@ func (body *Body) ToString() string {
 	bytes, _ := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(body)
 	return string(bytes)
 }
+
+type Headers struct {
+	keyvalues map[string][]string
+	keyvalue  map[string]string
+}
+
+func (headers *Headers) FromHttpRequest(r *http.Request) error {
+	for key, values := range r.Header {
+		headers.keyvalues[key] = values
+		headers.keyvalue[key] = values[0]
+	}
+	return nil
+}
+func (headers *Headers) ToString() string {
+	bytes, _ := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(headers.keyvalues)
+	return string(bytes)
+}
