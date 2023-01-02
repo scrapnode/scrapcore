56b6bb56af4f1f9ec5f81d14752599314c300190
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-02 10:52:29 +0700

    fix: could not set header after call WriteHeader

--- transport/http_response.go
+++ transport/http_response.go
@@ -25,18 +25,26 @@ func WriteString(writer http.ResponseWriter, data string) error {
 }
 
 func WriteErr400(writer http.ResponseWriter, err error) error {
+	// Changing the header map after a call to WriteHeader (or Write) has no effect
+	// unless the HTTP status code was of the 1xx class or the modified headers are trailers.
+	writer.Header().Set("content-type", "application/json")
+
 	writer.WriteHeader(http.StatusBadRequest)
 	data := map[string]interface{}{"error": err.Error()}
 	return WriteJSON(writer, data)
 }
 
 func WriteErr404(writer http.ResponseWriter, err error) error {
+	writer.Header().Set("content-type", "application/json")
+
 	writer.WriteHeader(http.StatusNotFound)
 	data := map[string]interface{}{"error": err.Error()}
 	return WriteJSON(writer, data)
 }
 
 func WriteErr500(writer http.ResponseWriter, err error) error {
+	writer.Header().Set("content-type", "application/json")
+
 	writer.WriteHeader(http.StatusInternalServerError)
 	data := map[string]interface{}{"error": err.Error()}
 	return WriteJSON(writer, data)
