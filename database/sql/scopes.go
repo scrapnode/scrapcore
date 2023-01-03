82837c03e4a191907b10e83732d67941fd8348e0
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-03 13:05:27 +0700

    refactor: don't use soft delete anymore

--- database/sql/scopes.go
+++ database/sql/scopes.go
@@ -1,18 +1,11 @@
 package sql
 
 import (
-	"github.com/benbjohnson/clock"
 	"gorm.io/gorm"
 )
 
 func UseWorkspace(ws string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Where("workspace_id = ?", ws)
-	}
-}
-
-func UseNotDeleted(c clock.Clock) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		return db.Where("deleted_at = 0 OR deleted_at > ?", c.Now().UTC().UnixMilli())
 	}
 }
