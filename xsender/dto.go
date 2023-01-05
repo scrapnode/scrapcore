2625dcbd522805d95efe394d922b97f72fff77d6
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-05 09:38:44 +0700

    feat: add some header helpers to xsender DTO

--- xsender/dto.go
+++ xsender/dto.go
@@ -1,15 +1,38 @@
 package xsender
 
+import (
+	jsoniter "github.com/json-iterator/go"
+	"net/http"
+)
+
 type Request struct {
-	Uri     string              `json:"uri"`
-	Method  string              `json:"method"`
-	Headers map[string][]string `json:"headers"`
-	Body    string              `json:"body"`
+	Uri     string      `json:"uri"`
+	Method  string      `json:"method"`
+	Headers http.Header `json:"headers"`
+	Body    string      `json:"body"`
+}
+
+func (req *Request) SetHeaders(data string) error {
+	return jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal([]byte(data), &req.Headers)
+}
+
+func (req *Request) GetHeaders(data string) (string, error) {
+	bytes, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(req.Headers)
+	return string(bytes), err
 }
 
 type Response struct {
-	Uri     string              `json:"uri"`
-	Status  int                 `json:"status"`
-	Headers map[string][]string `json:"headers"`
-	Body    string              `json:"body"`
+	Uri     string      `json:"uri"`
+	Status  int         `json:"status"`
+	Headers http.Header `json:"headers"`
+	Body    string      `json:"body"`
+}
+
+func (res *Response) SetHeaders(data string) error {
+	return jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal([]byte(data), &res.Headers)
+}
+
+func (res *Response) GetHeaders(data string) (string, error) {
+	bytes, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(res.Headers)
+	return string(bytes), err
 }
