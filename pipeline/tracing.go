4c487037a83571ff1bc21159b4a0808aeb7e827b
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-06 16:23:42 +0700

    refactor: change interface of UseTracing pipeline

--- pipeline/tracing.go
+++ pipeline/tracing.go
@@ -2,22 +2,30 @@ package pipeline
 
 import (
 	"context"
+	"fmt"
 	"github.com/scrapnode/scrapcore/utils"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/propagation"
 )
 
-func UseTracing(name, spanname string, pipeline Pipeline) Pipeline {
+type TracingConfig struct {
+	TraceName string
+	SpanName  string
+}
+
+func UseTracing(pipeline Pipeline, cfg *TracingConfig) Pipeline {
 	return func(next Pipe) Pipe {
 		return func(ctx context.Context) (context.Context, error) {
-			ctx, span := otel.Tracer(name).Start(ctx, spanname)
+			ctx, span := otel.Tracer(cfg.TraceName).Start(ctx, cfg.SpanName)
 
 			// use fake next function to make sure we don't trace subchain pipeline execute time
 			ctx, err := pipeline(func(ctx context.Context) (context.Context, error) {
 				return ctx, nil
 			})(ctx)
-			if err != nil {
+			if err == nil {
+				span.SetStatus(codes.Ok, fmt.Sprintf("%s.%s: ok", cfg.TraceName, cfg.SpanName))
+			} else {
 				span.SetStatus(codes.Error, err.Error())
 			}
 
