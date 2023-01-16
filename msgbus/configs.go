90c2832c875284629e2ce9649bbf21ba5694fd89
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-16 09:12:52 +0700

    refactor: add queue name to msgbus cfg

--- msgbus/configs.go
+++ msgbus/configs.go
@@ -1,9 +1,9 @@
 package msgbus
 
 type Configs struct {
-	Dsn    string `json:"dsn"`
-	Region string `json:"region"`
-	Name   string `json:"name"`
-
-	MaxRetry int `json:"max_retry"`
+	Dsn       string `json:"dsn"`
+	Region    string `json:"region"`
+	Name      string `json:"name"`
+	MaxRetry  int    `json:"max_retry"`
+	QueueName string `json:"queue_name"`
 }
